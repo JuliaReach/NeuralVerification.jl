17c7683dbf4680847c19ea0d3b02844b01a32e93
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-06-16 10:42:35 -0300

    add ai2z solver

---
+++ src/reachability/ai2z.jl
@@ -0,0 +1,47 @@
+"""
+    Ai2z <: AbstractSolver
+
+Ai2 performs over-approximated reachability analysis to compute the over-approximated output reachable set for a network.
+
+# Problem requirement
+1. Network: any depth, ReLU activation (more activations to be supported in the future)
+2. Input: Zonotope
+3. Output: Zonotope
+
+# Return
+`ReachabilityResult`
+
+# Method
+Reachability analysis using split and join.
+
+# Property
+Sound but not complete.
+
+# Reference
+T. Gehr, M. Mirman, D. Drashsler-Cohen, P. Tsankov, S. Chaudhuri, and M. Vechev,
+"Ai2: Safety and Robustness Certification of Neural Networks with Abstract Interpretation,"
+in *2018 IEEE Symposium on Security and Privacy (SP)*, 2018.
+"""
+struct Ai2z <: AbstractSolver end
+
+function solve(solver::Ai2z, problem::Problem)
+    reach = forward_network(solver, problem.network, problem.input)
+    return check_inclusion(reach, problem.output)
+end
+
+forward_layer(solver::Ai2z, layer::Layer, inputs::Vector{<:Zonotope}) = forward_layer.(solver, layer, inputs)
+
+function forward_layer(solver::Ai2z, layer::Layer, input::Zonotope)
+    outlinear = affine_map(layer, input)
+    relued_subsets = forward_partition(layer.activation, outlinear)
+    return overapproximate(convex_hull(relued_subsets), Zonotope)
+end
+
+# extend lazysets convex_hull to a vector of polytopes
+function LazySets.convex_hull(sets::Vector{<:Zonotope}; backend = CDDLib.Library())
+    hull = first(sets)
+    for P in sets
+        hull = convex_hull(hull, P)
+    end
+    return hull
+end
