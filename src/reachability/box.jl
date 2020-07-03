1b3a09cadb9a2c96a5ce1c2be0946a5cf3456685
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-07-03 11:46:58 -0300

    add Box() solver

---
+++ src/reachability/box.jl
@@ -0,0 +1,39 @@
+"""
+    Box <: AbstractSolver
+
+Box performs over-approximated reachability analysis to compute the over-approximated output reachable set for a network.
+
+# Problem requirement
+1. Network: any depth, ReLU activation (more activations to be supported in the future)
+2. Input: Hyperrectangle
+3. Output: Hyperrectangle
+
+# Return
+`ReachabilityResult`
+
+# Method
+Reachability analysis using using boxes.
+
+# Property
+Sound but not complete?.
+
+
+"""
+struct Box <: AbstractSolver end
+
+function solve(solver::Box, problem::Problem)
+    reach = forward_network(solver, problem.network, problem.input)
+    return check_inclusion(reach, problem.output)
+end
+
+forward_layer(solver::Box, layer::Layer, inputs::Vector{<:LazySet}) = forward_layer.(solver, layer, inputs)
+
+function forward_layer(solver::Box, layer::Layer, input::AbstractPolytope)
+    return forward_layer(solver, layer, overapproximate(input, Hyperrectangle))
+end
+
+function forward_layer(solver::Box, layer::Layer, input::Hyperrectangle)
+    outlinear = overapproximate(affine_map(layer, input), Hyperrectangle)
+    relued_subsets = forward_partition(layer.activation, outlinear)
+    return relued_subsets
+end
